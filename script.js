0c6fdb8f489fe558bf0ce8ed99424e15fa0dbc3a
plmn95 <plmn9595@gmail.com>
2024-08-22 23:29:52 +0300

    playable version

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-console.log("Hello World");
+console.log("obicham hui");
+
+function js() {
+    console.log("test");
+}
 
 function getComputerChoice() {
     random = Math.random()
@@ -13,4 +17,42 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     choice = prompt();
+    choice = choice.toLowerCase();
+    return choice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(CPU, player) {
+    player = getHumanChoice();
+    CPU = getComputerChoice();
+    console.log(player);
+    console.log(CPU);
+    if (player == "scissors" && CPU == "paper" ||
+        player == "paper" && CPU == "rock" ||
+        player == "rock" && CPU == "scissors") {
+            console.log("Player wins!");
+            humanScore = humanScore + 1;
+    } else if (player == "paper" && CPU == "scissors" ||
+            player == "scissors" && CPU == "rock" ||
+            player == "rock" && CPU == "paper") {
+                console.log("CPU wins!");
+                computerScore = computerScore + 1;
+    } else {
+                console.log("Draw!");
+    }
+}
+
+function playGame() {
+    for(let i = 0; i < 5; i++){
+        playRound();
+    }
+    if(humanScore > computerScore){
+        console.log("Player wins the game!");
+    }else if(computerScore > humanScore){
+        console.log("Computer wins the game!");
+    }else{
+        console.log("The game is draw!");
+    }
 }
